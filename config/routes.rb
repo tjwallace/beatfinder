c8e31fa57cd047c826a07988e61cdd4e4dbcfb91
Jeff Wallace <jeff@tjwallace.ca>
2010-08-27 23:02:07 +0200

    Fixup routes.

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Beatfinder::Application.routes.draw do |map|
-  devise_for :users, :path_names => { :sign_in => 'login', :sign_out => 'logout' }
-  match 'login', :to => 'devise/sessions#new', :as => "new_user_session"
-  match 'logout', :to  => 'devise/sessions#destroy', :as => "destroy_user_session"
-  match 'signup', :to => 'devise/registrations#new', :as => "new_user_registration"
+  devise_for :user do
+    get "/login" => "devise/sessions#new"
+    get "/logout" => "devise/sessions#destroy"
+    get "/signup" => "devise/registrations#new"
+  end
   
   resources :users do
     resources :playlists
